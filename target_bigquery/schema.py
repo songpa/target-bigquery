6f1b063fce25eb36dbced1464b458b14aace1a3a
Im, Seung <jim@keybank.com>
2022-03-23 13:38:41 -0400

    schema ref issue

--- target_bigquery/schema.py
+++ target_bigquery/schema.py
@@ -200,7 +200,7 @@ def convert_field_type(field_property):
     elif (("items" in field_property) and ("properties" not in field_property["items"])):
 
         # field_type_bigquery = conversion_dict[field_property['items']['type'][0]]
-        field_type_bigquery = "string"
+        field_type_bigquery = "STRING"
 
     else:
 
